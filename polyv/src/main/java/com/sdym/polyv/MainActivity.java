cff2965bb6237dad393544dd5d089dcfa2d91834
zangjy <2359270784@qq.com>
2022-09-14 13:33:42 +0800

    1.提交AndroidX版本

---
+++ polyv/src/main/java/com/sdym/polyv/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sdym.polyv;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.easefun.polyv.livecloudclass.SimpleJumpActivity;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
